4f68e37b2a3f6e03c23db61242051e4636967ab9
RaviTeja Vanjarapu <ravi.vanjarapu@gmail.com>
2023-02-18 22:21:47 +0530

    101. Symmetric Tree

---
+++ LeetCode/101. Symmetric Tree.py
@@ -0,0 +1,61 @@
+"""
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+
+
+Example 1:
+
+
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+
+
+Follow up: Could you solve it both recursively and iteratively?
+"""
+from turtle import right
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def recursive(left, right):
+            if not left and not right:
+                return True
+            elif not left or not right:
+                return False
+            return recursive(left.left, right.right) and recursive(left.right, right.left)
+
+        # return recursive(root.left, root.right)
+
+        q = [root, root]
+        while len(q) > 0:
+            left, right = q.pop(), q.pop()
+            if not left and not right:
+                continue
+            elif not left or not right:
+                return False
+            elif left.val != right.val:
+                return False
+            q.extend([left.left, right.right])
+            q.extend([left.right, right.left])
+        return True
+
