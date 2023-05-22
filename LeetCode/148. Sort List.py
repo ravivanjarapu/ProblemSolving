c8cbb415749941b44a423f4f7f32f04c34c49175
RaviTeja Vanjarapu <ravi.vanjarapu@gmail.com>
2023-05-22 22:52:25 +0530

    #148 Fix - Sort Linked list. Top down

--- LeetCode/148. Sort List.py
+++ LeetCode/148. Sort List.py
@@ -60,12 +60,14 @@ class Solution:
 
     def __merge_L_Lists(self, list1, list2):
         head = tail = ListNode()
-        if list1.val < list2.val:
-            tail.next = list1
-            list1 = list1.next
-        else:
-            tail.next = list2
-            list2 = list2.next
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
         if list1:
             tail.next = list1
         else:
