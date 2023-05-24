9ec2cc2e6c0196e68e79ee612e121a41b6c2d191
RaviTeja Vanjarapu <ravi.vanjarapu@gmail.com>
2023-05-24 19:26:40 +0530

    #150. Reverse Polish notation - Optimized

--- LeetCode/150. Evaluate Reverse Polish Notation.py
+++ LeetCode/150. Evaluate Reverse Polish Notation.py
@@ -48,16 +48,16 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         for c in tokens:
-            if c == '+':
-                stack.append(stack.pop() + stack.pop())
-            elif c == '-':
-                x, y = stack.pop(), stack.pop()
-                stack.append(y-x)
-            elif c == '*':
-                stack.append(stack.pop() * stack.pop())
-            elif c == '/':
-                x, y = stack.pop(), stack.pop()
-                stack.append(int(y / x))
-            else:
+            if c not in {'+', '-', '*', '/'}:
                 stack.append(int(c))
+            else:
+                num2, num1 = stack.pop(), stack.pop()
+                if c == '+':
+                    stack.append(num1 + num2)
+                elif c == '-':
+                    stack.append(num1 - num2)
+                elif c == '*':
+                    stack.append(num1 * num2)
+                else:
+                    stack.append(int(num1 / num2))
         return stack[0]
