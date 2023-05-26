d1624ec458ac88dec886354abba9391844051f75
RaviTeja Vanjarapu <ravi.vanjarapu@gmail.com>
2023-05-26 23:29:52 +0530

    #171 Excel sheet column number - Slightly Optimized

--- LeetCode/171. Excel Sheet Column Number.py
+++ LeetCode/171. Excel Sheet Column Number.py
@@ -37,11 +37,24 @@ columnTitle is in the range ["A", "FXSHRXW"].
 
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
-        length = len(columnTitle)
-
+        # length = len(columnTitle)
+        #
         result = 0
-        for i in range(length - 1, -1, -1):
-            letter = columnTitle[length - 1 - i]
-            ord_val = ord(letter) - 64  # ord(letter) - ord('A') + 1.  ord('A') is 65. So, - ord('A') + 1 is always 64
-            result += ord_val * (26 ** i)
+        # for i in range(length - 1, -1, -1):
+        #     letter = columnTitle[length - 1 - i]
+        #     ord_val = ord(letter) - 64  # ord(letter) - ord('A') + 1.  ord('A') is 65. So, - ord('A') + 1 is always 64
+        #     result += ord_val * (26 ** i)
+
+
+        '''The updated approach below should be faster than the previous implementation.
+        Exponentiation can be a computationally expensive operation, especially for larger powers.
+        In the updated approach, we avoid exponentiation and instead calculate the value iteratively by multiplying the 
+        existing result by 26 and adding the value for the current letter. This approach eliminates the need for 
+        exponentiation and reduces the number of mathematical operations required, making it more efficient in terms of 
+        time complexity.'''
+        for letter in columnTitle:
+            result *= 26
+            result += (ord(letter) - 64)
         return result
+
+
