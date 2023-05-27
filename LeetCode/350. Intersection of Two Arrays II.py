b385879f1bee6432a3b24d31838915618a1a05bf
RaviTeja Vanjarapu <ravi.vanjarapu@gmail.com>
2023-05-27 14:57:26 +0530

    #350 Intersection of two arrays II - Optimized

--- LeetCode/350. Intersection of Two Arrays II.py
+++ LeetCode/350. Intersection of Two Arrays II.py
@@ -29,13 +29,31 @@ What if elements of nums2 are stored on disk, and the memory is limited such tha
 from typing import List
 from collections import Counter
 
+
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums2_dict = dict(Counter(nums2))
+        if len(nums2) > len(nums1):  # This is to ensure space complexity is limited to the smallest array size
+            return self.intersect(nums2, nums1)
+
+        # nums2_dict = dict(Counter(nums2))
+        """
+        nums2_dict = defaultdict(int)
+        %timeit for i in nums2: nums2_dict[i] += 1
+        621 ns ± 62.1 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
+        
+        %timeit dict(Counter(nums2))
+        2.1 µs ± 223 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
+        
+        %timeit for i in nums2: nums2_dict[i] = nums2_dict.get(i, 0) + 1
+        633 ns ± 55.6 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
+        """
+        nums2_dict = {}
+        for i in nums2:
+            nums2_dict[i] = nums2_dict.get(i, 0) + 1
+
         result = []
         for i in nums1:
             if i in nums2_dict and nums2_dict[i] > 0:
                 result.append(i)
                 nums2_dict[i] -= 1
         return result
-
