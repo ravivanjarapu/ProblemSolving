8bfff6a8f7bdeb22b9e7825bf77b4784188487bd
RaviTeja Vanjarapu <ravi.vanjarapu@gmail.com>
2023-05-28 12:44:08 +0530

    #242 Valid Anagram - Complexity improved

--- LeetCode/242. Valid Anagram.py
+++ LeetCode/242. Valid Anagram.py
@@ -23,9 +23,19 @@ s and t consist of lowercase English letters.
 
 Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
 """
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) == len(t) and sorted(s) == sorted(t):
+        '''
+        return True if len(s) == len(t) and sorted(s) == sorted(t) else False:  # O( n log n ) time O(1) space
+        return True if len(s) == len(t) and Counter(s) = Counter(t) else False:  # O( 2n ) time O(2n) space
+        '''
+        # Below is O(2n) time and O(1) space
+        if len(s) == len(t):
+            s_counter = Counter(s)  # This is O(1) space since max length is 26 (alphabet count)
+            for i in t:
+                s_counter[i] -= 1
+                if s_counter[i] == -1:
+                    return False
             return True
-
         return False
