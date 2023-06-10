d540151854e73cc685cb36b6a3c73184ae9d9a42
RaviTeja Vanjarapu <ravi.vanjarapu@gmail.com>
2023-06-10 19:36:21 +0530

    #19 Remove Nth node from last - Dummy node way

--- LeetCode/19. Remove Nth Node From End of List.py
+++ LeetCode/19. Remove Nth Node From End of List.py
@@ -1,22 +1,15 @@
 """
 Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
-
-
 Example 1:
-
-
-Input: head = [1,2,3,4,5], n = 2
-Output: [1,2,3,5]
+    Input: head = [1,2,3,4,5], n = 2
+    Output: [1,2,3,5]
 Example 2:
-
-Input: head = [1], n = 1
-Output: []
+    Input: head = [1], n = 1
+    Output: []
 Example 3:
-
-Input: head = [1,2], n = 1
-Output: [1]
-
+    Input: head = [1,2], n = 1
+    Output: [1]
 
 Constraints:
 
@@ -41,20 +34,16 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        if not head:
-            return head
-        p, q = head, head
-        while q.next is not None and n > 0:
-            q = q.next
-            n -= 1
-        if q.next is None and n > 0:
-            return head.next
-        print('q:', q.val)
-        while q.next is not None:
-            p, q = p.next, q.next
-        print('p, q: ', p.val, q.val)
-        p.next = p.next.next
-        return head
+        right = head
+        for _ in range(n):  # 1 <= n <= sz. So, never goes out of bounds
+            right = right.next
+
+        dummy = ListNode(0, head)  # Dummy node added before head https://www.youtube.com/watch?v=XVuQxVej6y8 - NeetCode
+        left = dummy
+        while right:
+            left, right = left.next, right.next
+        left.next = left.next.next  # Removing the next node after left
+        return dummy.next
 
 
 obj = Solution()
