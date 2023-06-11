17ba32f9f362e9e0b27d2ef5c568d3924f623a48
RaviTeja Vanjarapu <ravi.vanjarapu@gmail.com>
2023-06-11 16:22:03 +0530

    #25 Reverse Nodes in k-group - Optimized with Iteration

--- LeetCode/25. Reverse Nodes in k-Group.py
+++ LeetCode/25. Reverse Nodes in k-Group.py
@@ -40,7 +40,13 @@ class ListNode:
 
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # Recursion
+        """
+            Recursion takes O(n) space for recursion stack
+            Iteration take O(1) space
+            https://leetcode.com/problems/reverse-nodes-in-k-group/editorial/ - Both recursion and iteration
+        """
+
+        '''# Recursion
         k_count, cur = 0, head
 
         while cur and k_count < k:  # Works until k > 0
@@ -51,7 +57,28 @@ class Solution:
             reversed_head = self.reverseList_upto_k_nodes(head, k)
             head.next = self.reverseKGroup(cur, k)
             return reversed_head
-        return head
+        return head'''
+
+        # Iteration
+        cur, new_head, k_tail = head, None, None
+        while cur:
+            k_count, cur = 0, head
+            while cur and k_count < k:  # Works until k > 0
+                cur = cur.next
+                k_count += 1  # Stops cur at k+1st node or None
+
+            if k == k_count:  # If full set of k nodes are available. If not, outer while loop exits too.
+                reversed_head = self.reverseList_upto_k_nodes(head, k)
+                if not new_head:
+                    new_head = reversed_head
+                if k_tail:
+                    k_tail.next = reversed_head
+                k_tail = head
+                head = cur
+        if k_tail:
+            k_tail.next = head
+
+        return new_head if new_head else head
 
     def reverseList_upto_k_nodes(self, head: Optional[ListNode], k) -> Optional[ListNode]:
         new_head, cur = None, head
